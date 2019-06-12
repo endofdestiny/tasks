21db0ffdee04ddfbd1d12d3e089320c641591157
End of Destiny <endofdestiny@live.co.uk>
2019-06-12 20:04:08 +0800

    Refactor home page view to use a template

--- functional_tests.py
+++ functional_tests.py
@@ -3,8 +3,6 @@ from selenium.webdriver.common.keys import Keys
 import unittest
 
 
-
-
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
@@ -14,17 +12,15 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-
     def test_can_start_a_task_list_and_retrieve_it_later(self):
         # Jen has heard about a cool task management app,
         # she checks out it's homepage
         self.browser.get("http://localhost:8000")
 
-
         # She notices the page title and header mention tasks
-        self.assertIn('Tasks', self.browser.title)
+        self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('Tasks', header_text)
+        self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item immediately
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -35,7 +31,6 @@ class NewVisitorTest(unittest.TestCase):
         # She types "Buy catfood" into the text box
         inputbox.send_keys('Buy catfood')
 
-
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy catfood" as an item on the tasks list
         inputbox.send_keys(Keys.ENTER)
@@ -44,21 +39,18 @@ class NewVisitorTest(unittest.TestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(any(row.text == "1: Buy Catfood" for row in rows))
 
-
         # There is still a text box inviting her to add another item
         # She adds "Feed Whiskers the cat"
         self.fail("Finish the test!")
+        # TODO finish test
 
         # The page updates again, and now she has both items on her list
-
 
         # Jen wonders whether the site will remember her list.
         # Then she sees that the site has generated a unique URL for her
         # There is some explanatory text to that effect
 
-
         # She visits that URL and her tasks are still there.
-
 
         # Happy, she leaves the site
 
